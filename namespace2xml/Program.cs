a74bc0d26ab5589954ffd8bb595905d0592a1543
stop-cran <stop-cran@list.ru>
2019-02-07 19:42:05 +0200

    test

---
+++ namespace2xml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace namespace2xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
